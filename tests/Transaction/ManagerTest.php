900556e5e6919ea1d2a2ad5dbae0ba4767693e16
Gilmar Pupo <g@g1mr.com>
2016-01-14 16:54:33 -0200

    fix tests

--- tests/Transaction/ManagerTest.php
+++ tests/Transaction/ManagerTest.php
@@ -26,7 +26,7 @@ class ManagerTest extends TestCaseAbstract
         $order = $this->factoryOrder();
         $transaction = $manager->createFromOrder($order);
         $this->assertInstanceOf('\Gpupo\SteloSdk\Transaction\Transaction', $transaction);
-        $this->assertSame('143800700149312', $transaction->getId());
+        $this->assertSame(143800700149312, $transaction->getId());
         $this->assertSame('https://carteira.hml.stelo.com.br/comprador/checkout?orderId=7K4PRkBxOdzx_9WZ8vioZQ', $transaction->getCheckoutUrl());
     }
 
@@ -36,7 +36,7 @@ class ManagerTest extends TestCaseAbstract
         $manager->setDryRun($this->factoryResponseFromFixture('fixtures/transaction.status.json'));
         $transaction = $manager->findById(143800246128360);
         $this->assertInstanceOf('\Gpupo\SteloSdk\Transaction\Transaction', $transaction);
-        $this->assertSame('143800246128360', $transaction->getId());
+        $this->assertSame(143800246128360, $transaction->getId());
     }
 
     public function testPermiteOCancelamentoDeUmaTransaçãoEspecífica()
